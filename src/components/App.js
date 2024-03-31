35521f73e65c24c4c63f2a86ff240275f3e91a61
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-03-31 11:20:51 +0000

    final sol

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,118 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
-import FlamesApp from "./FlamesApp";
+import React, { Component } from "react";
+import "../styles/App.css";
+
 class App extends Component {
-    render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name1: "",
+      name2: "",
+      relationship: "",
+    };
+  }
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-               <FlamesApp/>
-            </div>
-        )
+  calculateRelationship = () => {
+    const { name1, name2 } = this.state;
+
+    const nameA = name1.trim().split("");
+    const nameB = name2.trim().split("");
+
+    if (nameA.length === 0 && nameB.length === 0) {
+      this.setState({ relationship: "Please enter valid input" });
+      return;
     }
-}
 
+    const charCount = {};
+
+    for (let char of nameA) {
+      charCount[char] = (charCount[char] || 0) + 1;
+    }
+
+    let modified = "";
+    for (const char of nameB) {
+      if (charCount[char] && charCount[char] > 0) {
+        charCount[char]--;
+      } else {
+        modified += char;
+      }
+    }
+
+    for (const char in charCount) {
+      while (charCount[char] > 0) {
+        modified += char;
+        charCount[char]--;
+      }
+    }
+
+    const result = Math.floor(modified.length % 6);
+
+    switch (result) {
+      case 1:
+        this.setState({ relationship: "Friends" });
+        break;
+      case 2:
+        this.setState({ relationship: "Love" });
+        break;
+      case 3:
+        this.setState({ relationship: "Affection" });
+        break;
+      case 4:
+        this.setState({ relationship: "Marriage" });
+        break;
+      case 5:
+        this.setState({ relationship: "Enemy" });
+        break;
+      default:
+        this.setState({ relationship: "Siblings" });
+    }
+  };
+
+  clearForm = () => {
+    this.setState({
+      name1: "",
+      name2: "",
+      relationship: "",
+    });
+  };
+
+  render() {
+    const { name1, name2, relationship } = this.state;
+
+    return (
+      <>
+        <div id="main">
+          <input
+            type="text"
+            name="name1"
+            value={name1}
+            onChange={(e) => this.setState({ name1: e.target.value })}
+            placeholder="Enter first name"
+            data-testid="input1"
+          />
+          <br />
+          <input
+            type="text"
+            name="name2"
+            value={name2}
+            onChange={(e) => this.setState({ name2: e.target.value })}
+            placeholder="Enter second name"
+            data-testid="input2"
+          />
+          <br />
+          <button
+            onClick={this.calculateRelationship}
+            data-testid="calculate_relationship"
+          >
+            Calculate Relationship Future
+          </button>
+          <button onClick={this.clearForm} data-testid="clear">
+            Clear
+          </button>
+        </div>
+        <h3 data-testid="answer">{relationship}</h3>
+      </>
+    );
+  }
+}
 
 export default App;
